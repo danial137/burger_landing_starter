da1155c16f7f3acfcbce82aec052f97dd60baebd
danial137 <danial79fakhrabadi@gmail.com>
2025-12-06 19:17:53 +0330

    add features

--- app/components/Features/Features.tsx
+++ app/components/Features/Features.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import img1 from "@/public/images/f1.jpg"
+import Image from 'next/image'
 
 const Features = () => {
     return (
@@ -9,9 +11,14 @@ const Features = () => {
                 <span className='ml-5'>Care</span>
             </h1>
             {/* card */}
-
-
-            <div className='w-[90%] md:w-[80%] mt-[3rem] md:mt-[5rem] mb-[3rem]'></div>
+            <div className='w-[90%] md:w-[80%] mt-[3rem] md:mt-[5rem] mb-[3rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem]'>
+                {/* card1 */}
+                <div>
+                    <div className='p-6 hover:bg-white rounded-lg transition-all duration-200'>
+                        <Image src={img1} alt='img'/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
