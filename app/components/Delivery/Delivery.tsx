a892d6313c4a6658996602db9becdf1bdb4f6150
danial137 <danial79fakhrabadi@gmail.com>
2025-12-08 17:47:03 +0330

    add delivery details and icons to Delivery component

--- app/components/Delivery/Delivery.tsx
+++ app/components/Delivery/Delivery.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
 import DeliveryImg from "@/public/images/delivery.svg"
+import { RiEBike2Fill } from 'react-icons/ri'
+import { IoFastFood } from 'react-icons/io5'
+import { BsDoorOpen } from 'react-icons/bs'
 
 const Delivery = () => {
     return (
@@ -14,8 +17,23 @@ const Delivery = () => {
                 {/* text */}
                 <div>
                     <h1 className='text-[30px] uppercase md:text-[40px] lg:text-[50px] xl:text-[60px] font-bold leading-[3rem] md:leading-[4rem]'>Your <span className='text-red-600'>Favorite Burger</span> On the Way</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum amet odit atque facilis maxime consequatur doloribus asperiores accusamus voluptatum omnis, quos soluta cumque ut earum rem eaque, dignissimos nisi hic.</p>
+                    <p className='mt-[2rem] text-black text-[17px] text-opacity-70'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum amet odit atque facilis maxime consequatur doloribus asperiores accusamus voluptatum omnis, quos soluta cumque ut earum rem eaque, dignissimos nisi hic.</p>
+                    <div className='flex items-center space-x-3 mt-[2rem]'>
+                        <RiEBike2Fill className='w-[2rem] h-[2rem] text-red-600' />
+                        <h1 className='text-[18px] text-black font-medium'>Delivery in 30 minutes</h1>
+                    </div>
+                    <div className='flex items-center space-x-3 mt-[2rem]'>
+                        <IoFastFood className='w-[2rem] h-[2rem] text-red-600' />
+                        <h1 className='text-[18px] text-black font-medium'>Free Shiping from 751</h1>
+                    </div>
+                    <div className='flex items-center space-x-3 mt-[2rem]'>
+                        <BsDoorOpen className='w-[2rem] h-[2rem] text-red-600' />
+                        <h1 className='text-[18px] text-black font-medium'>
+                            Delivery on your door open
+                        </h1>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
