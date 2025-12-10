36cbc9003f9a65b9f236c9b3ab794276d5a5ae9d
danial137 <danial79fakhrabadi@gmail.com>
2025-12-10 15:24:46 +0330

    add FormBox component; enhance Reservition with reservation details and contact information

--- app/components/Reservition/Reservition.tsx
+++ app/components/Reservition/Reservition.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { BiPhone } from 'react-icons/bi'
+import FormBox from './FormBox'
 
 const Reservition = () => {
     return (
         <div className="pt-[5rem] bg-center bg-cover relative mt-[2rem] pb-[5rem] mb-[3rem] bg-[url('/images/bg-black.jpg')]">
             <div className='absolute w-full h-full bg-[#000000a6] top-0 left-0 right-0 bottom-0'></div>
-            <div className='[80%] relative z-[20] mx-auto grid grid-cols-1 lg:grid-cols-2 items-center gap-[3rem]'>
+            <div className='w-[80%] relative z-[20] mx-auto grid grid-cols-1 lg:grid-cols-2 items-center gap-[3rem]'>
                 {/* text */}
                 <div>
-                    <h1></h1>
+                    <h1 className='text-[30px md:text-[40px] lg:text-[50px] text-white font-bold leading-[3rem] md:leading-[4rem]'>DO YOU HAVE ANY DINNER PLAN TODAY? RESERVE YOUR TABLE</h1>
+                    <p className='text-[17px] mt-[1rem] text-white text-opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sit aspernatur quisquam fugiat similique quia cumque sunt ad quo harum sequi, pariatur provident, blanditiis est?</p>
+                    <div className='flex mt-[2rem] items-center space-x-4 '>
+                        <div className='w-[3rem] h-[3rem] bg-red-500 rounded-full flex items-center justify-center flex-col '>
+                            <BiPhone className='w-[1.7rem] h-[1.7rem] text-white' />
+                        </div>
+                        <div>
+                            <h1 className='text-[25px] text-white font-semibold'>Quick order 24/7</h1>
+                            <h1 className='text-yellow-300 text-[30px] font-bold'>+919 027 7976</h1>
+                        </div>
+                    </div>
                 </div>
 
                 {/* box */}
+                <FormBox />
             </div>
         </div>
     )
